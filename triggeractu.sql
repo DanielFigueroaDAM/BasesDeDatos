a86bad96fbf86f193ee91379549df66903b9a4a0
Daniel Figueroa <danifv02@gmail.com>
2025-02-13 10:09:30 +0100

    trigger ejercicio

---
+++ triggeractu.sql
@@ -0,0 +1,4 @@
+-- crea trigger chamado tactualizanumx tal que cando insira un novo xogador na taboa xogador actualize o numero de xogadores do seu equjipo en 
+-- 1 (sumarlle 1 ao campo  da taboa equipo)
+-- Expemplo de como sumar 1 ao numero de xogadores (cammpo numx) do equipo 1
+-- update equipo set numx=numx+1 where codequ='e1';
