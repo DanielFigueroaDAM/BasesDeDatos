3ad83eb5f637e54818840d64ad668ace9fc7adb3
Daniel Figueroa <danifv02@gmail.com>
2025-02-17 09:17:25 +0100

    trigger de klingon terminado

--- triggers/tklingon.sql
+++ triggers/tklingon.sql
@@ -1,5 +1,25 @@
 /*impedir inserir un klingon se xa está en humanos ou en vulcanos
 insert into klingon values ('p1',null,'pla1'); --->rexeitada, este persoaxe xa está en humanos
 insert into klingon values ('p2',null,'pla1');--->rexeitada, este persoaxe xa está en vulcanos
-insert into klingon values ('p5',null,'pla1');--->aceptada */
+insert into klingon values ('p5',null,'pla1');--->aceptada insercion */
 
+drop trigger if exists tklingon on klingon; 
+create or replace function ft_klingon() returns trigger language plpgsql as $$
+declare 
+
+conth integer;
+contv integer;
+begin
+select count(*) into conth from humanos where codper=new.codper;
+select count(*) into contv from vulcanos where codper=new.codper;
+if(conth>=1) then
+raise exception 'rexeitada, este persoaxe xa está en humanos';
+elsif (contv>=1) then
+raise exception 'rexeitada, este persoaxe xa está en vulcan os';
+else
+raise notice 'sivai';
+end if;
+return new;
+end; $$;
+
+create trigger tklingon before insert on klingon for each row execute procedure ft_klingon();
