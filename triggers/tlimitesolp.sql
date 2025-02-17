5475ed9e3867216c6c8ad04d8cd493a4f1ca8864
Daniel Figueroa <danifv02@gmail.com>
2025-02-17 10:32:22 +0100

    tlimitesolp teminado

--- triggers/tlimitesolp.sql
+++ triggers/tlimitesolp.sql
@@ -9,7 +9,32 @@ insert into personaxes values ('p48','zira','soldado','p46');
 
 insert into personaxes values ('p49','nerf','cabo','p21');
 'rexistro inserido'*/
-drop trigger if exists tlimitesolp on personaxes;
-create or replace function ft_limitesolp() returns trigger language plpgsql as $$
-declare
+DROP TRIGGER IF EXISTS tlimitesolp ON personaxes;
 
+CREATE OR REPLACE FUNCTION ft_limitesolp() 
+RETURNS TRIGGER 
+LANGUAGE plpgsql 
+AS $$
+DECLARE
+    tipo_xefe TEXT;
+BEGIN
+    -- Obtener el tipo del jefe (si existe)
+    SELECT graduacion INTO tipo_xefe 
+    FROM personaxes 
+    WHERE codper = NEW.codper2;
+
+    -- Si el nuevo personaje es un soldado y su jefe también lo es, bloqueamos la inserción
+    IF NEW.graduacion = 'soldado' AND tipo_xefe = 'soldado' THEN
+        RAISE EXCEPTION 'Este personaxe é un soldado e non debe ter por xefe a outro soldado';
+    ELSE
+        RAISE NOTICE 'Rexistro inserido';
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER tlimitesolp 
+BEFORE INSERT ON personaxes 
+FOR EACH ROW 
+EXECUTE FUNCTION ft_limitesolp();
