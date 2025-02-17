09cdfdaf1ad022ae5ab8e1c5887651364277b864
Daniel Figueroa <danifv02@gmail.com>
2025-02-17 09:05:48 +0100

    enunciado trigger strartreck

---
+++ triggers/tklingon.sql
@@ -0,0 +1,5 @@
+/*impedir inserir un klingon se xa está en humanos ou en vulcanos
+insert into klingon values ('p1',null,'pla1'); --->rexeitada, este persoaxe xa está en humanos
+insert into klingon values ('p2',null,'pla1');--->rexeitada, este persoaxe xa está en vulcanos
+insert into klingon values ('p5',null,'pla1');--->aceptada */
+
