394acf79ee6af64db6322eefe1b28863e541f96e
Daniel Figueroa <danifv02@gmail.com>
2025-03-17 11:38:55 +0100

    ejercicio hospital2

--- phospital2.sql
+++ phospital2.sql
@@ -7,13 +7,15 @@ medicovc varchar;
 fila2 record;
 codhosp varchar;
 begin
-    for fila in (select * from hospital) loop
-        resultado=resultado||fila.nomh||E'\n';
-        select codh into codhosp from hospital where nomh=fila.nomh;
-        for fila2 in select * from hosp1 where codh=codhosp loop
-            select distinct codm into medicovc from hosp1 where 
-        end loop;
+for fila in (select * from hospital) loop
+    resultado = resultado || fila.nomh || E'\n';
+    select codh into codhosp from hospital where nomh = fila.nomh;
+    for fila2 in (select distinct codm from hosp1 where codh = codhosp) loop
+        select nomm into medicovc from medico where codm = fila2.codm;
+        
+        resultado = resultado || '    ' || medicovc || E'\n';
     end loop;
+end loop;
 
 
 raise notice '%',resultado;
